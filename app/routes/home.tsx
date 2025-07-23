e8316af7820007401cfbbf9a84addb9df5e11b94
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-07-23 21:23:51 +0530

    feat: creating the nav bar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,12 +10,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
       <section className="section"></section>
-    <div className="page-heading">
-      <h1>Track Your Application & Resume Ratings</h1>
-      <h2>Review your submission and check AI-powered feedback.</h2>
-
-    </div>
-  </main>
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submission and check AI-powered feedback.</h2>
+      </div>
+    </main>
+  );
 }
