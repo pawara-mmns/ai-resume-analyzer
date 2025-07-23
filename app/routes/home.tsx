4786bf25c0ab42b2fc7636aebbba5cd08e24ad06
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-07-23 16:51:30 +0530

    feat: add header and background desgin to Home-page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,18 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="section"></section>
+    <div className="page-heading">
+      <h1>Track Your Application & Resume Ratings</h1>
+      <h2>Review your submission and check AI-powered feedback.</h2>
+
+    </div>
+  </main>
 }
